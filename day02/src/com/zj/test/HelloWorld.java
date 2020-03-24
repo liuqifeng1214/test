6a337eeb9222d39e5892d159a78356b27ac8fca1
lqf <zj_liuqifeng@163.com>
2020-03-25 00:47:35 +0800

    test 第一次开始

---
+++ day02/src/com/zj/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zj.test;
+
+/**
+ * @author
+ * @creatime 2020-03-25-00:40
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("test开始");
+    }
+}
